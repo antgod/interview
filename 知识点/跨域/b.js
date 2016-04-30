0f1275e107fb312f367d480a442138c7dcacbc2a
lihongji <lihongji@meituan.com>
2016-04-30 10:11:32 +0800

    add

---
+++ 知识点/跨域/b.js
@@ -0,0 +1,21 @@
+function GetRequest() {
+  
+  var url = location.search; //获取url中"?"符后的字串
+   var theRequest = new Object();
+   if (url.indexOf("?") != -1) {
+      var str = url.substr(1);
+      strs = str.split("&");
+      for(var i = 0; i < strs.length; i ++) {
+         theRequest[strs[i].split("=")[0]]=(strs[i].split("=")[1]);
+      }
+   }
+   return theRequest;
+}
+
+var attrs=GetRequest();
+for(var attr in attrs){
+	console.log(attr);
+	console.log(attrs[attr]);
+	eval(attrs[attr])({a:1,b:2});
+}
+
---
+++ 面试题-按公司/美月面试/a.js
@@ -0,0 +1,4 @@
+/**
+ * Created by lihongji on 2015/9/20.
+ */
+console.log(2);
